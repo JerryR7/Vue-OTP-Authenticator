cfb0122f998897904179db1a99fcaa4f0c4b05ed
Jerry Lin <vp780412@gmail.com>
2023-12-03 05:23:34 +0800

    refactor: refactor OTP verification logic in Vue component
    - Refactor the `length` property definition to a single line in `OtpVerify.vue`
    - Introduce `validateInput` function to ensure each input character is a digit in `OtpVerify.vue`
    - Simplify `handleEnter` by delegating logic to new `isBackspaceOrDelete` and `handleBackspaceOrDelete` functions in `OtpVerify.vue`
    - Create `isBackspaceOrDelete` to check for backspace or delete keypress in `OtpVerify.vue`
    - Implement `handleBackspaceOrDelete` to manage backspace or delete actions in `OtpVerify.vue`
    - Add `handleRegularInput` to focus on next input after a number is entered in `OtpVerify.vue`
    - Remove redundant null checks and simplify condition in `checkOTP` function in `OtpVerify.vue`
    - Simplify HTTP headers object in `verifyOTP` fetch request in `OtpVerify.vue`
    - Add early exit for non-OK server response in `verifyOTP` function in `OtpVerify.vue`
    - Handle successful OTP verification and emit token in `verifyOTP` function in `OtpVerify.vue`
    - Streamline error handling and set loading state to false in `verifyOTP` function in `OtpVerify.vue`
    - Simplify pasting logic to handle OTP paste in one operation in `handlePaste` function in `OtpVerify.vue`
    - Add `@input` event binding for input validation on each input

--- src/components/OtpVerify.vue
+++ src/components/OtpVerify.vue
@@ -2,10 +2,7 @@
 import {ref, onMounted, inject} from 'vue'
 
 const otpProps = defineProps({
-  length: {
-    type: Number,
-    default: 4
-  }
+  length: {type: Number, default: 4}
 })
 
 const otp = ref(new Array(otpProps.length).fill(''))
@@ -18,50 +15,48 @@ onMounted(() => {
   otp.value.fill('')
 })
 
-function handleEnter(e: KeyboardEvent, i: number) {
-  const children = container.value.children
-  const keyPressed = e.key
-
-  if (i > 0 &&(keyPressed === 'Backspace' || keyPressed === 'Delete')) {
-    if (otp.value[i] != null && i == otpProps.length - 1) {
-      otp.value[i] = null
-      return
-    }
-    otp.value[i] = null
-    children[i - 1].focus()
-    return
-  } else {
-    const matched = keyPressed.match(/^[0-9]$/)
-    if (!matched) {
-      if (i < otpProps.length - 1 || otp.value[i] === null) {
-        otp.value[i] = null;
-      }
-    } else {
-      otp.value[i] = e.key;
-      if (i < otpProps.length - 1) {
-        children[i + 1].focus();
-      }
-    }
-    checkOTP()
+function validateInput(index: number) {
+  if (!/^[0-9]$/.test(otp.value[index])) {
+    otp.value[index] = ''
   }
 }
 
+function handleEnter(e: KeyboardEvent, i: number) {
+  isBackspaceOrDelete(e) ? handleBackspaceOrDelete(i) : handleRegularInput(e, i)
+}
+
+function isBackspaceOrDelete(e: KeyboardEvent): boolean {
+  return e.key === 'Backspace' || e.key === 'Delete'
+}
+
+function handleBackspaceOrDelete(i: number) {
+  if (i == otpProps.length - 1 && otp.value[i] != null) {
+    otp.value[i] = null
+  } else {
+    otp.value[i] = null
+    container.value.children[i - 1]?.focus()
+  }
+}
+
+function handleRegularInput(e: KeyboardEvent, i: number) {
+  if (/^[0-9]$/.test(e.key)) {
+    otp.value[i] = e.key
+    if (i < otpProps.length - 1) {
+      container.value.children[i + 1].focus()
+    }
+  } else {
+    otp.value[i] = ''
+  }
+  checkOTP()
+}
+
 function checkOTP() {
-  let isComplete  = otp.value.every(val => val !== null && val !== '')
+  let isComplete = otp.value.every(val => val !== null && val != '')
 
   if (isComplete) {
     setLoading(true)
     emit('entered', otp.value.join(''))
-    verifyOTP(otp.value.join('')).then(isValid => {
-      if (isValid) {
-        otpError.value = ''
-      } else {
-        setLoading(false)
-        otpError.value = 'Invalid OTP code. Please try again.'
-        otp.value.fill(null)
-        container.value.children[0].focus();
-      }
-    })
+    verifyOTP(otp.value.join(''))
   }
 }
 
@@ -69,37 +64,35 @@ async function verifyOTP(code: string) {
   try {
     const response = await fetch('/api/verify', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ code })
-    });
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({code})
+    })
 
-    const data = await response.json();
+    if (!response.ok) otpError.value = 'Server response not OK'
 
+    const data = await response.json()
     if (data.valid) {
-      emit('token', data.token);
-      return data.valid
+      emit('token', data.token)
     } else {
-      otpError.value = 'Invalid OTP code. Please try again.';
+      otpError.value = 'Invalid OTP code. Please try again.'
+      otp.value.fill('')
+      container.value.children[0].focus()
     }
   } catch (error) {
-    console.error("Error verifying OTP", error);
-    otpError.value = 'An error occurred while verifying. Please try again.';
+    console.error("Error verifying OTP", error)
+    otpError.value = 'An error occurred while verifying. Please try again.'
+  } finally {
+    setLoading(false)
   }
 }
-
 
 function handlePaste(event: ClipboardEvent) {
   const clipboardData = event.clipboardData
   const pastedData = clipboardData?.getData('Text').trim()
 
-  if (pastedData != null && pastedData.length === otpProps.length && /^\d+$/.test(pastedData)) {
-    pastedData.split('').forEach((digit, index) => {
-      otp.value[index] = digit
-      container.value.children[index].focus()
-    })
-
+  if (pastedData && pastedData.length === otpProps.length && /^\d+$/.test(pastedData)) {
+    otp.value = pastedData.split('')
+    container.value.children[otpProps.length - 1].focus()
     checkOTP()
   }
 }
@@ -111,8 +104,17 @@ function handlePaste(event: ClipboardEvent) {
     <div class="otp-container">
       <h3 class="otp-title">Enter verification</h3>
       <div ref="container" class="input-group">
-        <input v-for="n in length" :key="n" @keyup="(e) => handleEnter(e, n-1)" @paste="handlePaste" v-model="otp[n-1]"
-               class="input" type="text" maxlength="1" :autofocus="n === 1">
+        <input
+            v-for="n in length"
+            :key="n"
+            @keyup="(e) => handleEnter(e, n-1)"
+            @paste="handlePaste"
+            @input="validateInput(n-1)"
+            v-model="otp[n-1]"
+            class="input"
+            type="text"
+            maxlength="1"
+            :autofocus="n === 1">
       </div>
       <div v-if="otpError" class="error-message">{{ otpError }}</div>
     </div>
@@ -127,6 +129,7 @@ function handlePaste(event: ClipboardEvent) {
   flex-direction: column;
   gap: 1rem;
 }
+
 .otp-title {
   font-size: 2rem;
 }
@@ -141,10 +144,6 @@ function handlePaste(event: ClipboardEvent) {
   height: 12rem;
   text-align: center;
   font-size: 2rem;
-}
-
-.no-pointer-events {
-  pointer-events: none;
 }
 
 .error-message {
